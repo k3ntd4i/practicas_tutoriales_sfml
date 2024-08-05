990ed63a77eb4e44e637bfe4d5735b060ad6b06c
k3ntd4i <ackar_quentdie48@outlook.com>
2024-08-05 18:49:52 -0500

    Ejemplo figura

--- main.cpp
+++ main.cpp
@@ -1,12 +1,39 @@
-#include <iostream>
-#include "SFML/Graphics.hpp"
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    sf::RenderWindow ventana(sf::VideoMode(800,600), "Prueba");
-    sf::sleep(sf::milliseconds(3000));
+    sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
+    sf::CircleShape shape(250.f);
+    shape.setFillColor(sf::Color::Cyan);
 
-    std::cout << "Chao, World!" << '\n';
+    while (window.isOpen())
+    {
+        sf::Event event;
+
+        while (window.pollEvent(event))
+        {
+            switch (event.type)
+            {
+            case sf::Event::Closed:
+                window.close();
+                break;
+
+            case sf::Event::KeyPressed:
+                if (event.key.code == sf::Keyboard::Escape)
+                {
+                    window.close();
+                }
+                break;
+
+            default:
+                break;
+            }
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 
     return 0;
 }
